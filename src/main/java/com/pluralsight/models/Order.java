c31ccaba1432d56bd73d7ba9879cd2c35c6d07d8
Daja Miller <dajamiller96@gmail.com>
2025-11-13 10:56:57 -0800

    order class, generics list instantiated and methods for total and adding items resolves #15

--- src/main/java/com/pluralsight/models/Order.java
+++ src/main/java/com/pluralsight/models/Order.java
@@ -1,4 +1,32 @@
 package com.pluralsight.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+
+    // private variables
+    private String customerName;
+    private List<MenuItem> menuItems;
+
+    //constructor
+    public Order(String customerName) {
+        this.customerName = customerName;
+        menuItems = new ArrayList<>();
+    }
+
+    //getters/setters
+
+
+    public void addItem(MenuItem menuItem) {
+        this.menuItems.add(menuItem);
+    }
+
+    public double calculateTotalPrice() {
+        double totalPrice = 0.00;
+        for (MenuItem menuItem : menuItems) {
+            totalPrice += menuItem.getPrice();
+        }
+        return totalPrice;// sum of all items
+    }
 }
