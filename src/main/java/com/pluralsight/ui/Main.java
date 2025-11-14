f6aeb4b19b3b436a21bd2977d647f471a02d73cc
Daja Miller <dajamiller96@gmail.com>
2025-11-13 23:20:30 -0800

    moved main arguments to own class

---
+++ src/main/java/com/pluralsight/ui/Main.java
@@ -0,0 +1,8 @@
+package com.pluralsight.ui;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        userInterface.showHomeScreen();
+    }
+}
